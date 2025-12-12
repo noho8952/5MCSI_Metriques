8c617c81340bcaa16f54fe80ce336d00400881d0
noho8952 <noha.talhi@eleve.isep.fr>
2025-12-12 10:21:31 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,27 +1,39 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template_string, render_template, jsonify
+from flask import render_template
+from flask import json
+from datetime import datetime
 from urllib.request import urlopen
-import json
+import sqlite3
 
-app = Flask(name)
+app = Flask(name) 
+
+@app.route("/contact/")
+def MaPremiereAPI():
+    return "<h2>Ma page de contact</h2>"
 
 @app.route('/tawarano/')
 def meteo():
-    response = urlopen(
-        'https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx'
-    )
+    response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
     raw_content = response.read()
     json_content = json.loads(raw_content.decode('utf-8'))
-
     results = []
-    for element in json_content.get('list', []):
-        dt_value = element.get('dt')
-        temp_celsius = element.get('main', {}).get('temp') - 273.15
-        results.append({
-            'Jour': dt_value,
-            'temp': temp_celsius
-        })
-
+    for list_element in json_content.get('list', []):
+        dt_value = list_element.get('dt')
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
+        results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
 
+@app.route("/rapport/")
+def mongraphique():
+    return render_template("graphique.html")
+
+@app.route("/histogramme/")
+def monhistogramme():
+    return render_template("histogramme.html")
+
+@app.route('/')
+def hello_world():
+    return render_template('hello.html')
+
 if name == "main":
-    app.run(debug=True)
+  app.run(debug=True)
